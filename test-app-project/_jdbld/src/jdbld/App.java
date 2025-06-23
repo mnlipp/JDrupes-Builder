43925d5424a88080c408bf394bce340f1724f1ea
Michael N. Lipp <mnl@mnl.de>
2025-06-23 13:24:14 +0200

    Generate (most) Eclipse configuration files.

--- test-app-project/_jdbld/src/jdbld/App.java
+++ test-app-project/_jdbld/src/jdbld/App.java
@@ -1,22 +1,18 @@
 package jdbld;
 
-import java.nio.file.Path;
 import java.util.EnumSet;
-
 import static org.jdrupes.builder.api.Intend.*;
-
 import org.jdrupes.builder.api.Intend;
 import org.jdrupes.builder.core.AbstractProject;
+import org.jdrupes.builder.java.JavaProject;
 import org.jdrupes.builder.java.UberJarGenerator;
-import org.jdrupes.builder.java.JavaCompiler;
 
-public class App extends AbstractProject {
+public class App extends AbstractProject implements JavaProject {
 
     public App() {
         super(name("app"));
         dependency(project(Base1.class), Consume);
         dependency(project(Base2.class), Consume);
-        generator(JavaCompiler::new).addSources(Path.of("src"), "**/*.java");
         dependency(new UberJarGenerator(this)
             .addAll(providers(EnumSet.of(Consume, Supply))), Intend.Forward);
     }
---
+++ test-app-project/_jdbld/src/jdbld/Base2.java
@@ -0,0 +1,12 @@
+package jdbld;
+
+import org.jdrupes.builder.core.AbstractProject;
+import org.jdrupes.builder.java.JavaProject;
+
+public class Base2 extends AbstractProject implements JavaProject {
+
+    public Base2() {
+        super(name("base2"));
+    }
+
+}
