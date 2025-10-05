04b5e40164f01de43a3845ba34d94d7dbb75a9d1
Michael N. Lipp <mnl@mnl.de>
2025-10-05 13:18:13 +0200

    Cleanup generated subartifacts.

--- mvnrepo/src/org/jdrupes/builder/mvnrepo/MvnPublicationGenerator.java
+++ mvnrepo/src/org/jdrupes/builder/mvnrepo/MvnPublicationGenerator.java
@@ -29,6 +29,8 @@ import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.Security;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 import org.apache.maven.model.building.DefaultModelBuilderFactory;
@@ -46,6 +48,9 @@ import org.bouncycastle.openpgp.PGPUtil;
 import org.bouncycastle.openpgp.operator.jcajce.JcaKeyFingerprintCalculator;
 import org.bouncycastle.openpgp.operator.jcajce.JcaPGPContentSignerBuilder;
 import org.bouncycastle.openpgp.operator.jcajce.JcePBESecretKeyDecryptorBuilder;
+import org.eclipse.aether.AbstractRepositoryListener;
+import org.eclipse.aether.DefaultRepositorySystemSession;
+import org.eclipse.aether.RepositoryEvent;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.deployment.DeployRequest;
@@ -54,20 +59,18 @@ import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.util.artifact.SubArtifact;
 import org.eclipse.aether.util.repository.AuthenticationBuilder;
 import org.jdrupes.builder.api.BuildException;
-import org.jdrupes.builder.api.FileResource;
 import static org.jdrupes.builder.api.Intend.*;
 import org.jdrupes.builder.api.Project;
 import org.jdrupes.builder.api.Resource;
 import org.jdrupes.builder.api.ResourceRequest;
 import static org.jdrupes.builder.api.ResourceRequest.requestFor;
 import org.jdrupes.builder.core.AbstractGenerator;
-import org.jdrupes.builder.java.JarGenerator;
 import org.jdrupes.builder.java.JavadocJarFile;
 import org.jdrupes.builder.java.LibraryJarFile;
 import org.jdrupes.builder.java.SourcesJarFile;
 import static org.jdrupes.builder.mvnrepo.MvnRepoTypes.*;
 
-@SuppressWarnings("PMD.CouplingBetweenObjects")
+@SuppressWarnings({ "PMD.CouplingBetweenObjects", "PMD.ExcessiveImports" })
 public class MvnPublicationGenerator extends AbstractGenerator {
 
     private URI repositoryUri;
@@ -79,26 +82,56 @@ public class MvnPublicationGenerator extends AbstractGenerator {
     private PGPPublicKey publicKey;
     private Supplier<Path> artifactDirectory
         = () -> project().buildDirectory().resolve("publications/maven");
+    private boolean keepSubArtifacts;
 
+    /// Creates a new Maven publication generator.
+    ///
+    /// @param project the project
+    ///
     public MvnPublicationGenerator(Project project) {
         super(project);
         repositoryUri = URI.create(MvnRepoLookup.rootContext()
             .remoteRepositories().get(0).getUrl());
     }
 
+    /// Sets the Maven repository URI.
+    ///
+    /// @param uri the repository URI
+    /// @return the maven publication generator
+    ///
     public MvnPublicationGenerator repository(URI uri) {
         this.repositoryUri = uri;
         return this;
     }
 
+    /// Sets the Maven snapshot repository URI.
+    ///
+    /// @param uri the snapshot repository URI
+    /// @return the maven publication generator
+    ///
     public MvnPublicationGenerator snapshotRepository(URI uri) {
         this.snapshotUri = uri;
         return this;
     }
 
+    /// Sets the Maven repository credentials.
+    ///
+    /// @param user the username
+    /// @param pass the password
+    /// @return the maven publication generator
+    ///
     public MvnPublicationGenerator credentials(String user, String pass) {
         this.repoUser = user;
         this.repoPass = pass;
+        return this;
+    }
+
+    /// Keep generated sub artifacts.
+    ///
+    /// @return the mvn publication generator
+    ///
+    public MvnPublicationGenerator keepSubArtifacts() {
+        keepSubArtifacts = true;
         return this;
     }
 
@@ -124,6 +157,7 @@ public class MvnPublicationGenerator extends AbstractGenerator {
     public MvnPublicationGenerator artifactDirectory(Path directory) {
         if (directory == null) {
             this.artifactDirectory = () -> null;
+            return this;
         }
         this.artifactDirectory
             = () -> project().buildDirectory().resolve(directory);
@@ -206,41 +240,67 @@ public class MvnPublicationGenerator extends AbstractGenerator {
         }
     }
 
+    private record Deployable(Artifact artifact, boolean temporary) {
+    }
+
     @SuppressWarnings("PMD.AvoidDuplicateLiterals")
     private Stream<?> deploy(PomFile pomResource, LibraryJarFile jarResource,
-            SourcesJarFile srcsFile, JavadocJarFile jdFile)
+            SourcesJarFile srcsJar, JavadocJarFile javadocJar)
             throws ModelBuildingException, DeploymentException {
         var mainArtifact = mainArtifact(pomResource);
-        var isSnapshot = mainArtifact.isSnapshot();
-        var repo = new RemoteRepository.Builder(
-            "mine", "default",
-            (isSnapshot ? snapshotUri : repositoryUri)
-                .toString()).setAuthentication(new AuthenticationBuilder()
-                    .addUsername(repoUser).addPassword(repoPass)
-                    .build()).build();
-        var deployReq = new DeployRequest().setRepository(repo);
         if (artifactDirectory() != null) {
             artifactDirectory().toFile().mkdirs();
         }
-        addArtifact(deployReq, new SubArtifact(mainArtifact, "", "pom"),
-            pomResource);
-        addArtifact(deployReq, new SubArtifact(mainArtifact, "", "jar"),
-            jarResource);
-        if (srcsFile != null) {
-            addArtifact(deployReq,
-                new SubArtifact(mainArtifact, "sources", "jar"), srcsFile);
+        List<Deployable> toDeploy = new ArrayList<>();
+        addWithGenerated(toDeploy, new SubArtifact(mainArtifact, "", "pom",
+            pomResource.path().toFile()));
+        addWithGenerated(toDeploy, new SubArtifact(mainArtifact, "", "jar",
+            jarResource.path().toFile()));
+        if (srcsJar != null) {
+            addWithGenerated(toDeploy, new SubArtifact(mainArtifact, "sources",
+                "jar", srcsJar.path().toFile()));
         }
-        if (jdFile != null) {
-            addArtifact(deployReq,
-                new SubArtifact(mainArtifact, "javadoc", "jar"), jdFile);
+        if (javadocJar != null) {
+            addWithGenerated(toDeploy, new SubArtifact(mainArtifact, "javadoc",
+                "jar", javadocJar.path().toFile()));
         }
 
         // Now deploy everything
         @SuppressWarnings("PMD.CloseResource")
         var context = MvnRepoLookup.rootContext();
-        // TODO disable temporarily
-        var result = context.repositorySystem().deploy(
-            context.repositorySystemSession(), deployReq);
+        var session = new DefaultRepositorySystemSession(
+            context.repositorySystemSession());
+        session.setRepositoryListener(new AbstractRepositoryListener() {
+            @Override
+            public void artifactDeployed(RepositoryEvent event) {
+                if (!"jar".equals(event.getArtifact().getExtension())) {
+                    return;
+                }
+                log.info(() -> "Deployed: " + event.getArtifact());
+            }
+
+            @Override
+            public void metadataDeployed(RepositoryEvent event) {
+                log.info(() -> "Deployed: " + event.getMetadata());
+            }
+
+        });
+        var isSnapshot = mainArtifact.isSnapshot();
+        var repo = new RemoteRepository.Builder(
+            "mine", "default",
+            (isSnapshot ? snapshotUri : repositoryUri)
+                .toString()).setAuthentication(new AuthenticationBuilder()
+                    .addUsername(repoUser).addPassword(repoPass).build())
+                    .build();
+        var deployReq = new DeployRequest().setRepository(repo);
+        toDeploy.stream().map(d -> d.artifact).forEach(deployReq::addArtifact);
+        var result = context.repositorySystem().deploy(session, deployReq);
+        if (!keepSubArtifacts) {
+            toDeploy.stream().filter(Deployable::temporary).forEach(d -> {
+                d.artifact().getFile().delete();
+            });
+
+        }
         return Stream.of(project().newResource(MvnPublicationType,
             mainArtifact.getGroupId() + ":" + mainArtifact.getArtifactId()
                 + ":" + mainArtifact.getVersion()));
@@ -256,15 +316,17 @@ public class MvnPublicationGenerator extends AbstractGenerator {
             "jar", model.getVersion());
     }
 
-    private void addArtifact(DeployRequest deployReq, Artifact artifact,
-            FileResource resource) {
-        deployReq.addArtifact(artifact.setFile(resource.path().toFile()));
+    private void addWithGenerated(List<Deployable> toDeploy,
+            Artifact artifact) {
+        // Add main artifact
+        toDeploy.add(new Deployable(artifact, false));
 
         // Generate .md5 and .sha1 checksum files
+        var artifactFile = artifact.getFile().toPath();
         try {
             MessageDigest md5 = MessageDigest.getInstance("MD5");
             MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
-            try (var fis = Files.newInputStream(resource.path())) {
+            try (var fis = Files.newInputStream(artifactFile)) {
                 byte[] buffer = new byte[8192];
                 while (true) {
                     int read = fis.read(buffer);
@@ -275,40 +337,40 @@ public class MvnPublicationGenerator extends AbstractGenerator {
                     sha1.update(buffer, 0, read);
                 }
             }
-            var fileName = resource.path().getFileName().toString();
+            var fileName = artifactFile.getFileName().toString();
 
             // Handle generated md5
             var md5Hex = toHex(md5.digest()) + "  " + fileName + "\n";
-            var md5Path = destinationPath(resource, fileName + ".md5");
+            var md5Path = destinationPath(artifactFile, fileName + ".md5");
             Files.writeString(md5Path, md5Hex);
-            deployReq.addArtifact(new SubArtifact(artifact, "*", "*.md5")
-                .setFile(md5Path.toFile()));
+            toDeploy.add(new Deployable(new SubArtifact(artifact, "*", "*.md5",
+                md5Path.toFile()), true));
 
             // Handle generated sha1
             String sha1Hex = toHex(sha1.digest()) + "  " + fileName + "\n";
-            var sha1Path = destinationPath(resource, fileName + ".sha1");
+            var sha1Path = destinationPath(artifactFile, fileName + ".sha1");
             Files.writeString(sha1Path, sha1Hex);
-            deployReq.addArtifact(new SubArtifact(artifact, "*", "*.sha1")
-                .setFile(sha1Path.toFile()));
+            toDeploy.add(new Deployable(new SubArtifact(artifact, "*", "*.sha1",
+                sha1Path.toFile()), true));
 
             // Add signature as yet another artifact
-            var sigPath = signResource(resource);
-            deployReq.addArtifact(new SubArtifact(artifact, "*", "*.asc")
-                .setFile(sigPath.toFile()));
+            var sigPath = signResource(artifactFile);
+            toDeploy.add(new Deployable(new SubArtifact(artifact, "*", "*.asc",
+                sigPath.toFile()), true));
         } catch (NoSuchAlgorithmException | IOException | PGPException e) {
             throw new BuildException(e);
         }
     }
 
-    private Path destinationPath(FileResource base, String fileName) {
+    private Path destinationPath(Path base, String fileName) {
         var dir = artifactDirectory();
         if (dir == null) {
-            base.path().resolveSibling(fileName);
+            base.resolveSibling(fileName);
         }
         return dir.resolve(fileName);
     }
 
-    public static String toHex(byte[] bytes) {
+    private static String toHex(byte[] bytes) {
         char[] hexDigits = "0123456789abcdef".toCharArray();
         char[] result = new char[bytes.length * 2];
 
@@ -350,16 +412,16 @@ public class MvnPublicationGenerator extends AbstractGenerator {
 
     }
 
-    private Path signResource(FileResource resource)
+    private Path signResource(Path resource)
             throws PGPException, IOException {
         initSigning();
         PGPSignatureGenerator signatureGenerator = new PGPSignatureGenerator(
             signerBuilder, publicKey);
         signatureGenerator.init(PGPSignature.BINARY_DOCUMENT, privateKey);
         var sigPath = destinationPath(resource,
-            resource.path().getFileName() + ".asc");
+            resource.getFileName() + ".asc");
         try (InputStream fileInput = new BufferedInputStream(
-            Files.newInputStream(resource.path()));
+            Files.newInputStream(resource));
                 OutputStream sigOut
                     = new ArmoredOutputStream(Files.newOutputStream(sigPath))) {
             byte[] buffer = new byte[8192];
