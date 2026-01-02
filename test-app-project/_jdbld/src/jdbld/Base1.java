2dd43a416ab48f741b5b922bbf96a271c15a2099
Michael N. Lipp <mnl@mnl.de>
2026-01-02 18:37:46 +0100

    Nest test project.

--- test-app-project/_jdbld/src/jdbld/Base1.java
+++ test-app-project/_jdbld/src/jdbld/Base1.java
@@ -1,5 +1,8 @@
 package jdbld;
 
+import static org.jdrupes.builder.api.Intend.Consume;
+
+import org.jdrupes.builder.api.MergedTestProject;
 import org.jdrupes.builder.core.AbstractProject;
 import org.jdrupes.builder.java.JavaProject;
 
@@ -7,6 +10,15 @@ public class Base1 extends AbstractProject implements JavaProject {
 
     public Base1() {
         super(name("base1"));
+    }
+
+    public static class Base1Test extends AbstractProject
+            implements JavaProject, MergedTestProject {
+
+        public Base1Test() {
+            super(parent(Base1.class));
+            dependency(Consume, project(Base1.class));
+        }
     }
 
 }
