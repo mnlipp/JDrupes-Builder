d9749ce3a9f9dfb57655d11f1fc172fe0f3c1881
Michael N. Lipp <mnl@mnl.de>
2026-04-24 15:42:56 +0200

    Re-enable parallelism.

--- java/src/org/jdrupes/builder/java/LibraryBuilder.java
+++ java/src/org/jdrupes/builder/java/LibraryBuilder.java
@@ -146,12 +146,14 @@ public class LibraryBuilder extends JarBuilder
     ///
     protected void collectFromProviders(
             Map<Path, Resources<IOResource>> contents) {
+        // Not using the intermediate toList.parallelStream causes this
+        // to hang. Not sure why.
         contentProviders().stream().map(p -> p.resources(
             of(ClassTreeType).using(Supply))).flatMap(s -> s)
-            .forEach(t -> collect(contents, t));
+            .toList().parallelStream().forEach(t -> collect(contents, t));
         contentProviders().stream().map(p -> p.resources(
             of(JavaResourceTreeType).using(Supply))).flatMap(s -> s)
-            .forEach(t -> collect(contents, t));
+            .toList().parallelStream().forEach(t -> collect(contents, t));
     }
 
     @Override
